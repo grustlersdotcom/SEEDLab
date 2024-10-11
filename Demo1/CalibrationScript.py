43af03b4f7f1544178e437d95b235a0846c44812
Broberts12 <118421554+Broberts12@users.noreply.github.com>
2024-10-11 13:33:36 -0600

    Update CalibrationScript.py

--- Demo1/CalibrationScript.py
+++ Demo1/CalibrationScript.py
@@ -6,22 +6,24 @@ import glob
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
  
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((4*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:4].T.reshape(-1,2)
  
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
  
-images = glob.glob('*.jpg')
- 
+images = glob.glob('./captured_images/*.png')
+
 for fname in images:
+
     img = cv.imread(fname)
+    #print(img.shape)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
  
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
- 
+    ret, corners = cv.findChessboardCorners(gray, (7,4), None)
+    #print(corners)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
@@ -30,11 +32,14 @@ for fname in images:
         imgpoints.append(corners2)
  
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (7,4), corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(500)
     
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    print(ret,mtx,dist,rvecs,tvecs)
- 
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+#print(f"MTX: {mtx} \n Dist: {dist}")
+img = cv.imread("./captured_images/photo_2024-10-04_13-31-43.png")
+h, w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+print(newcameramtx)
 cv.destroyAllWindows()
